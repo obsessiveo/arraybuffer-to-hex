d4eb52e09a72f78b0d8c31126bd3004217e3d0b6
Obsessiveo <obsessiveo@protonmail.com>
2022-09-06 10:33:11 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+/**
+ * Convert an array of bytes to a hex string
+ * @param buffer the array buffer to convert
+ * @returns the hex string
+ */
+export function arrayBufferToHex(buffer: ArrayBuffer): string {
+  return Array.prototype.map
+    .call(new Uint8Array(buffer), (x) => ('00' + x.toString(16)).slice(-2))
+    .join('');
+}
+
+// Language: typescript
+// Path: index.ts
